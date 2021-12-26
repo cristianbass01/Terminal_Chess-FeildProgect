1b0830a86e2c51f37eb13a90470a1b76dd3b88fb
Coniglio Spaziale <giovanni.cinel@studenti.unipd>
2021-12-26 17:02:32 +0100

    ultime modifiche

--- pezzi/pedone.h
+++ pezzi/pedone.h
@@ -1,7 +1,12 @@
 #ifndef PEDONE_H_
 #define PEDONE_H_
 
-class Pedone{
+#include "pezzo.h"
+
+class Pedone: public Pezzo{
+  public:
+    Pedone(int rig, int col) : riga{rig}, colonna {col}{ }
+    
 
 }
 #endif // PEDONE_H_
