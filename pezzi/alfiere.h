9ff81f334fbc54554b09d27025368a6f5ac7561d
Coniglio Spaziale <giovanni.cinel@studenti.unipd>
2021-12-26 18:52:39 +0100

    sistemati

--- pezzi/alfiere.h
+++ pezzi/alfiere.h
@@ -6,6 +6,8 @@
 class Alfiere : public Pezzo {
   public:
     Alfiere(int rig, int col, Colore color);
+    void mossa(int r, int c) override {} //metodo virtuale puro che sposta il pezzo
+    bool mossa_valida(int r, int c) override {return false;}
 };
 
 #endif // ALFIERE_H
