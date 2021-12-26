b4d59633342fe567c2924ccee67d9e7e005998a8
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-26 15:02:32 +0100

    scritta classe pezzo

--- pezzo.h
+++ pezzo.h
@@ -1,5 +1,20 @@
 #ifndef PEZZO_H_
 #define PEZZO_H_
 
+class Pezzo { //classe virtuale pura
+    public:
+        void mossa(int r, int c) = 0; //metodo virtuale puro che sposta il pezzo
+        bool valida(int r, int c) = 0; //metodo virtuale puro che controlla se la mossa è valida
+    protected:
+        //DA DECIDERE SE USARE STRUCT O NO
+        int riga;
+        int colonna;
+        
+        char figura; //contiene la figura che viene rappresentata nella scachiera
+        
+        class enum Colore {
+            bianco, nero        // bianco=0, nero=1
+        }
+    }
 
 #endif  // PEZZO_H_
