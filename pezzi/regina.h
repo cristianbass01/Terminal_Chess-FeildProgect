94043fea93f84797e90243013d2f7761528440bf
mister <cristianbassotto21@gmail.com>
2021-12-26 18:53:27 +0100

    override

--- pezzi/regina.h
+++ pezzi/regina.h
@@ -5,7 +5,9 @@
 
 class Regina : public Pezzo{
   public:
-     Regina(int rig, int col, Colore color);
+    Regina(int rig, int col, Colore color);
+    void mossa(int r, int c) override {} //metodo virtuale puro che sposta il pezzo
+    bool mossa_valida(int r, int c) override {return false;}
 };
 
 #endif // REGINA_H
