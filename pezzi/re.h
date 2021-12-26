37bdbd2987f631f3b710e79047884e685316b52b
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-26 18:12:47 +0100

    costruttore di Re migliorato

--- pezzi/re.h
+++ pezzi/re.h
@@ -5,10 +5,14 @@
 
 class Re : public Pezzo {
   public:
-    Re(int rig, int col, int color) { 
+    Re(int rig, int col, Pezzo::Colore color) { 
       riga = rig;
       colonna = col;
-      colore = Pezzo::Colore(color);
+      colore = color;
+      if(colore == Colore::nero)
+        figura = 'R';
+      else
+        figura = 'r';
     }
     void mossa(int r, int c) override {} //metodo virtuale puro che sposta il pezzo
     bool mossa_valida(int r, int c) override {return false;}
