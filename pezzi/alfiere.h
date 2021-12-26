190533ee2662a6a0994352e38858b26731645dfd
Coniglio Spaziale <giovanni.cinel@studenti.unipd>
2021-12-26 17:28:29 +0100

    Costruttori

--- pezzi/alfiere.h
+++ pezzi/alfiere.h
@@ -1,8 +1,11 @@
 #ifndef ALFIERE_H_
 #define ALFIERE_H_
 
-class Alfiere {
-     
+#include "pezzo.h"
+
+class Alfiere: public Pezzo {
+  public:
+    Alfiere(int rig, int col) : riga{rig}, colonna {col}{ }
 }
 
 #endif // ALFIERE_H_
