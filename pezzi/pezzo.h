d80eb320604f6cd4af0f9be43937bd337178ffbe
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-27 16:09:39 +0100

    aggiornato pezzo

--- pezzi/pezzo.h
+++ pezzi/pezzo.h
@@ -6,13 +6,13 @@
 class Pezzo { //classe virtuale pura
   public:
     virtual void mossa(Casella posizione) = 0; //metodo virtuale puro che sposta il pezzo
-    virtual bool mossa_valida(int r, int c) = 0; //metodo virtuale puro che controlla se la mossa è valida
-    char get_figura() { return figura; }
+    virtual bool mossa_valida(Casella posizione) = 0; //metodo virtuale puro che controlla se la mossa è valida
+    char get_figura() { return figura_; }
     enum class Colore {
       bianco, nero        // bianco=0, nero=1
     };
   protected:
-    //DA DECIDERE SE USARE STRUCT O NO
+    //classe casella che contiene riga e colonna
     Casella posizione_;
         
     char figura_; //contiene la figura che viene rappresentata nella scachiera
