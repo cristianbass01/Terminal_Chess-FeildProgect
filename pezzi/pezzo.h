4d3e5a5940e2d2468611a876176ae300a6cf8655
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-27 15:56:13 +0100

    risolto conflitto

--- pezzi/pezzo.h
+++ pezzi/pezzo.h
@@ -15,10 +15,6 @@ class Pezzo { //classe virtuale pura
     int colonna;
         
     char figura; //contiene la figura che viene rappresentata nella scachiera
-<<<<<<< Updated upstream
-=======
-    
->>>>>>> Stashed changes
     Colore colore;
 };
 
