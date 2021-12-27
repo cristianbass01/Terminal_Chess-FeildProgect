d46e4a52ed5d3d1f35e0b6bef4be607fdef8cf0f
mister <cristianbassotto21@gmail.com>
2021-12-27 16:40:39 +0100

    re risolto

--- pezzi/re.h
+++ pezzi/re.h
@@ -5,15 +5,9 @@
 
 class Re : public Pezzo {
   public:
-<<<<<<< Updated upstream
-    Re(Casella posizione, Pezzo::Colore colore);
-    void mossa(Casella posizione) override {} //metodo virtuale puro che sposta il pezzo
-    bool mossa_valida(Casella posizione) override {return false;}
-=======
     Re(Casella posizione, Pezzo::Colore color);
     void mossa(Casella posizione) override; //metodo virtuale puro che sposta il pezzo
     bool mossa_valida(Casella posizione) override;
->>>>>>> Stashed changes
 };
 
 #endif // RE_H
