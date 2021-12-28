a062f23ef944da8c2f3253d7b170794d4f2eba7d
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-28 19:49:00 +0100

    movimento pedone (passa la mano a cristian)

--- pezzi/pedone.cpp
+++ pezzi/pedone.cpp
@@ -9,5 +9,27 @@ Pedone::Pedone(Casella posizione, Colore colore) {
 }
 
 bool Pedone::mossa_valida(Casella posizione_finale, char** scacchiera){
-  return false;
+  //parte per i biachi
+  if(colore_ == Colore::bianco) {
+    //caso in cui arretra
+    if(posizione_.get_riga() >= posizione_finale.get_riga())
+      return false;
+
+    //caso in cui si trova nella posizione iniziale
+    if((posizione_finale.get_riga() - posizione_.get_riga()) != 1) {
+      if(posizione_.get_riga() != 1)
+        return false;
+      if((posizione_finale.get_riga() - posizione_.get_riga()) != 2)
+        return false;
+    }
+
+    
+
+    //caso in cui abbia un pezzo davanti
+
+  }
+  
+  
+  
+  
 }
