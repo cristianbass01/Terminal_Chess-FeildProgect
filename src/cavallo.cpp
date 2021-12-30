fbd4cffa0d8ca626ed4e9dd19802a0dcd40672e2
Coniglio Spaziale <giovanni.cinel@studenti.unipd>
2021-12-30 22:14:35 +0100

    cavallo

--- src/cavallo.cpp
+++ src/cavallo.cpp
@@ -13,5 +13,13 @@ Cavallo::Cavallo(Casella posizione, Colore colore){
 bool Cavallo::mossa_valida(Casella posizione_finale, Scacchiera& scacchiera){
   if((*(scacchiera.get_casella(posizione_finale))).get_colore() == colore_)
     return false;
+  int delta_riga = posizione_.get_riga() - posizione_finale.get_riga();
+  int delta_colonna = posizione_.get_colonna() - posizione_finale.get_colonna());
+  if( delta_colonna < 0)
+    delta_colonna *= -1; 
+  if( delta_riga < 0)
+    delta_riga *= -1; 
+  if(((delta_colonna == 2)&&(delta_riga == 1))||((delta_colonna == 1)&&(delta_riga == 2)))
+    return true;
   return false;
 }
