1dc319ab246d173ff81f3bffa34d5fa9191dc0b5
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2021-12-30 23:33:57 +0100

    commentato e sistemato alfiere

--- src/alfiere.cpp
+++ src/alfiere.cpp
@@ -11,12 +11,20 @@ Alfiere::Alfiere(Casella posizione, Colore colore) {
 }
 
 bool Alfiere::mossa_valida(Casella posizione_finale, Scacchiera& scacchiera){
+  //calcolo di delta riga e delta colonna
   int delta_riga = posizione_.get_riga() - posizione_finale.get_riga();
   int delta_colonna = posizione_.get_colonna() - posizione_finale.get_colonna();
+
+  //verifica che non ci sia una pedina dello stesso colore nella posizione finale
   if((*(scacchiera.get_casella(posizione_finale))).get_colore() == colore_)
     return false;
+
+  //temp_*** sono variabili temporanee che consentono di determinare di quanto si è spostato 
+  //fin adesso l'alfiere
   int temp_driga;
   int temp_dcolonna;
+  
+  //rimozione ultima casella da temp_d***
   if(delta_riga > 0)
     temp_driga = delta_riga -1;
   else
@@ -25,12 +33,19 @@ bool Alfiere::mossa_valida(Casella posizione_finale, Scacchiera& scacchiera){
     temp_dcolonna = delta_colonna -1;
   else
     temp_dcolonna = delta_colonna +1;
+  
+  //verifica che le posizioni in diagonale tra posizione finale e inziale siano libere
   Casella temp_casella;
   while(temp_dcolonna != 0){
+    //crea una casella (richiesto per poter utilizzare scacchiera.get_casella())
     temp_casella.set_colonna(posizione_.get_colonna() + temp_dcolonna);
     temp_casella.set_riga(posizione_.get_riga() + temp_driga);
-    if((scacchiera.get_casella(temp_casella)) != nullptr )
+
+    //verifica che non ci siano pedine in mezzo alla diagonale di movimento
+    if(scacchiera.get_casella(temp_casella) != nullptr )
       return false;
+    
+    //modifica temp_d*** a seconda del verso in cui ci si muove
     if(temp_driga > 0)
       temp_driga--;
     else
@@ -40,10 +55,14 @@ bool Alfiere::mossa_valida(Casella posizione_finale, Scacchiera& scacchiera){
     else
       temp_dcolonna++;
   }
+
+  //calcolo del modulo di delta_riga e delta_colonna
   if( delta_colonna < 0)
     delta_colonna *= -1; 
   if( delta_riga < 0)
     delta_riga *= -1; 
+
+  //verifica che il movimento sia in diagonale controllando che moduli siano uguali
   if( delta_colonna != delta_riga)
     return false;
   return true;
