5f67483a0e02fbb890bdbcd56208be9d81d03695
mister <cristianbassotto21@gmail.com>
2021-12-30 20:30:40 +0100

    Pulizia

---
+++ build/CMakeFiles/Feild_Progect.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/alfiere.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/alfiere.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/casella.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/casella.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/cavallo.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/cavallo.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/computer.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/computer.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/eccezione.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/eccezione.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/main.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/main.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/pedone.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/pedone.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/re.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/re.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/regina.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/regina.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/scacchiera.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/scacchiera.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/torre.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/torre.cpp.o"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/src/umano.cpp" "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/Feild_Progect.dir/src/umano.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/alfiere.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/cavallo.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/pedone.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/re.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/regina.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/torre.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/casella.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/computer.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/eccezione.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/scacchiera.dir/DependInfo.cmake"
+  "/home/cristian/OneDrive/2 anno/Laboratorio di programmazione/Feild_Progect/build/CMakeFiles/umano.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
