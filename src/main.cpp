d68207255469d799bfaadbe2091b8eeb5b2ea8c3
Coniglio Spaziale <giovanni.cinel@studenti.unipd>
2022-01-02 15:31:33 +0100

    NNI

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
   Scacchiera test;
 
-  Casella cavallo(0,1);  
+  /**Casella cavallo(0,1);  
   Casella cavallo1(2,2);
   test.mossa(cavallo, cavallo1);
   std::cout << "\n";
@@ -25,15 +25,15 @@ int main() {
   std::cout << "\n";
 
 
-  //Casella alfiere(0,5);  
-  //Casella alfiere1(1,4);
-  //test.mossa(alfiere, alfiere1);
-  test.mossa(Casella(0,), Casella(1,4));
+  Casella alfiere(0,5);  
+  Casella alfiere1(1,4);
+  test.mossa(alfiere, alfiere1);
+  test.mossa(Casella(0,3), Casella(1,4));
   test.stampa();
   std::cout << "\n";
-  
+  **/
 
-  /** 
+  
   bool colore = static_cast<bool>(rand() % 2); //scelta randomica dei colori dei giocatori
   Umano giocatore_1(&test, static_cast<Pezzo::Colore>(colore));
   Umano giocatore_2(&test, static_cast<Pezzo::Colore>(!colore));
@@ -46,6 +46,6 @@ int main() {
     giocatore_1.gioca();
     giocatore_2.gioca();
   }
-  **/
+  
   return 0;
 }
