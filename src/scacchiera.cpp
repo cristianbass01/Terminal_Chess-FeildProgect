2047cafe8b100a8b1cdc49554d74e228ee5da655
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2022-01-04 22:26:03 +0100

    .

--- src/scacchiera.cpp
+++ src/scacchiera.cpp
@@ -249,56 +249,6 @@ std::string Scacchiera::stringa_per_mappa() {
       if(pezzo_corrente == nullptr)
         temp.push_back(' ');
       else {
-        /*
-        //caso in cui il pezzo da inserire è un pedone bianco
-        if(pezzo_corrente->get_figura() == 'p') {
-          if(static_cast<Pedone*>(pezzo_corrente)->en_passant_valid_)
-            temp.push_back('f');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-
-        //caso in cui il pezzo da inserire è il pedone nero
-        if(pezzo_corrente->get_figura() == 'P') {
-          if(static_cast<Pedone*>(pezzo_corrente)->en_passant_valid_)
-            temp.push_back('F');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-
-        //caso in cui il pezzo da inserire è un re bianco
-        if(pezzo_corrente->get_figura() == 'r') {
-          if(static_cast<Re*>(pezzo_corrente)->get_arrocco_valido())
-            temp.push_back('k');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-
-        //caso in cui il pezzo da inserire è un re nero
-        if(pezzo_corrente->get_figura() == 'R') {
-          if(static_cast<Re*>(pezzo_corrente)->get_arrocco_valido())
-            temp.push_back('K');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-
-        //caso in cui il pezzo da inserire è un torre bianca
-        if(pezzo_corrente->get_figura() == 't') {
-          if(static_cast<Torre*>(pezzo_corrente)->get_arrocco_valido())
-            temp.push_back('g');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-
-        //caso in cui il pezzo da inserire è un torre nera
-        if(pezzo_corrente->get_figura() == 'T') {
-          if(static_cast<Torre*>(pezzo_corrente)->get_arrocco_valido())
-            temp.push_back('G');
-          else
-            temp.push_back(pezzo_corrente->get_figura());
-        }
-        */
-
         switch(pezzo_corrente->get_figura()) {
           case 'p': //caso di pedone bianco
             if(static_cast<Pedone*>(pezzo_corrente)->en_passant_valid_)
