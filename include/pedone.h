a6b4c3077fe80b69af4f0e9e78cbdb19903609a7
mister <cristianbassotto21@gmail.com>
2022-01-06 22:17:00 +0100

    Mossa_salto = -1

--- include/pedone.h
+++ include/pedone.h
@@ -19,7 +19,7 @@ class Pedone : public Pezzo {
     int get_mossa_salto() {return mossa_salto;}
   private:
     //membro che salva il numero della mossa alla quale è stato fatto il doppio salto iniziale
-    int mossa_salto;
+    int mossa_salto = -1;
 
     //metodo che controlla se la mossa è valida (SENZA controllo scacco)
     bool mossa_valida(Casella posizione, Scacchiera& scacchiera) override;
