80e60c8c253927aa585bf3aacbd0c2b825ac8bff
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2022-01-06 22:27:24 +0100

    rimosso en_passant valid e messo il  mossa salto al suo posto

--- src/pedone.cpp
+++ src/pedone.cpp
@@ -6,6 +6,7 @@ Pedone::Pedone(Casella posizione, Colore colore) {
   //inizializzate variabili membro ereditate da pezzo
   posizione_ = posizione;
   colore_ = colore;
+  mossa_salto = -1; //mossa salto assumerà valore positivo solo se viene fatto un salto di 2 
 
   //inizializzata figura sulla base del colore del pezzo
   if(colore == Colore::nero)
@@ -68,12 +69,10 @@ bool Pedone::mossa(Casella posizione_finale, Scacchiera& scacchiera){
   if(posizione_finale.get_riga() == posizione_.get_riga() + indice_colore){ // faccio un pass in avanti o laterale
     if((posizione_finale.get_colonna() == posizione_.get_colonna()) && (scacchiera.get_casella(posizione_finale) == nullptr)){ // colonna uguale (non mangia pezzi)
       posizione_ = posizione_finale;
-      en_passant_valid_ = false;
       return true;
     }
     if((abs(posizione_finale.get_colonna() - posizione_.get_colonna()) == 1) && (scacchiera.get_casella(posizione_finale) != nullptr)){ // si sposta di una colonna e una riga e mangia
       posizione_ = posizione_finale;
-      en_passant_valid_ = false;
       return true;
     }
     if((abs(posizione_finale.get_colonna() - posizione_.get_colonna()) == 1) && (scacchiera.get_casella(Casella(posizione_.get_riga(), posizione_finale.get_colonna())) != nullptr)){ // mossa speciale en passant
@@ -86,14 +85,14 @@ bool Pedone::mossa(Casella posizione_finale, Scacchiera& scacchiera){
   {
     // controllo che sia la prima mossa bianca e che non ci siano pezzi nelle due caselle davanti al pedone
     if((colore_ == Pezzo::Colore::bianco) && (posizione_.get_riga() == 1) && (scacchiera.get_casella(Casella(posizione_.get_riga() + 1, posizione_.get_colonna())) == nullptr) && (scacchiera.get_casella(posizione_finale)) == nullptr){
-      en_passant_valid_ = true;
+      mossa_salto = scacchiera.get_conta_mosse(); //settato mossa_salto alla mossa corrente
       posizione_ = posizione_finale;
       return true;
     }
 
     // controllo che sia la prima mossa nera e che non ci siano pezzi nelle due caselle davanti al pedone
     if((colore_ == Pezzo::Colore::nero) && (posizione_.get_riga() == 6) && (scacchiera.get_casella(Casella(posizione_.get_riga() - 1, posizione_.get_colonna())) == nullptr) && (scacchiera.get_casella(posizione_finale)) == nullptr){
-      en_passant_valid_ = true;
+      mossa_salto = scacchiera.get_conta_mosse(); //settato mossa_salto alla mossa corrente
       posizione_ = posizione_finale;
       return true;
     }
