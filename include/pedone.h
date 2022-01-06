0881fbff477a062b1456a40b0271cabdd21d8c91
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2022-01-06 23:06:15 +0100

    manca da sistemare un pezzetto

--- include/pedone.h
+++ include/pedone.h
@@ -10,12 +10,6 @@ class Pedone : public Pezzo {
     //costruttore
     Pedone(Casella posizione, Colore colore);
 
-    //metodo che sposta il pezzo
-    int mossa(Casella posizione, Scacchiera& scacchiera) override;
-
-    //metodo che controlla se la mossa è valida (ANCHE controllo scacco)
-    int simulazione_mossa(Casella posizione, Scacchiera& scacchiera) override; 
-
     int get_mossa_salto() {return mossa_salto;}
   private:
     //membro che salva il numero della mossa alla quale è stato fatto il doppio salto iniziale
