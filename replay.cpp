6ebd680e1287dc8ed5f3c4c51b9c5b2fe48a9935
GianlucaNordio <gianluca.nordio@studenti.unipd.it>
2022-01-07 19:56:31 +0100

    in teoria finito replay

--- replay.cpp
+++ replay.cpp
@@ -100,6 +100,9 @@ int main(int argc, char *argv[]) {
 
         if(scacchiera_.mossa(iniziale, finale)) 
           throw Eccezione("[Eccezione::Log_Errato]");
+
+        if(scacchiera_.pezzi_insufficienti())
+          throw Eccezione("[Eccezione::Patta_Insufficenza_di_Pezzi]");
         
         constexpr int NUMERO_MILLISECONDI_ATTESA = 1000;
         std::this_thread::sleep_for(std::chrono::milliseconds(NUMERO_MILLISECONDI_ATTESA)); 
@@ -108,8 +111,17 @@ int main(int argc, char *argv[]) {
 
         //verifica che non sia scaccomatto
         Pezzo::Colore colore_avversario_ = (colore_ == Pezzo::Colore::nero) ? Pezzo::Colore::bianco : Pezzo::Colore::nero ;
-        if(scacchiera_.scaccomatto(colore_avversario_))
+        if(scacchiera_.scaccomatto(colore_avversario_)) {
+          std::cout<<"Ha vinto il giocatore ";
+          if(colore_ == Pezzo::Colore::bianco)
+            std::cout<<"bianco!"<<std::endl;
+          else
+            std::cout<<"nero!"<<std::endl;
           throw Eccezione("[Eccezione::Scaccomatto]");
+        }
+        
+        if(scacchiera_.get_conta_mosse() >= 50)
+          throw Eccezione("[Eccezione::Patta_Mosse]");
 
         
       }
@@ -124,100 +136,20 @@ int main(int argc, char *argv[]) {
     std::string fine_partita; 
     if((e.errore()).compare("[Eccezione::Log_Errato]") == 0) // fine partita perché formato log non corretto
       fine_partita = "Log_Errato";
+
     if((e.errore()).compare("[Eccezione::Patta_Stallo]") == 0) // gestione patta per stallo
       fine_partita = "Patta_Stallo";
-    if((e.errore()).compare("[Eccezione::Patta_Materiale]") == 0) // gestione patta per materiale insufficiente NON FATTA
+
+    if((e.errore()).compare("[Eccezione::Patta_Materiale]") == 0) // gestione patta per materiale insufficiente 
       fine_partita = "Patta_Insufficienza di materiale";
     
-    if((e.errore()).compare("[Eccezione::Patta_Posizione]") == 0) // gestione patta posizione ripetuta
-      fine_partita = "Patta_Posizione ripetuta";
     if((e.errore()).compare("[Eccezione::Patta_Mosse]") == 0) // gestione patta mossa
       fine_partita = "Patta_Gioco fermo (mosse)";
-    if((e.errore()).compare("[Eccezione::Scaccomatto]") == 0){ // gestione scaccomatto NON FATTA
+
+    if((e.errore()).compare("[Eccezione::Scaccomatto]") == 0){ // gestione scaccomatto 
       fine_partita = "Scaccomatto";
-      vincitore = giocatore_1.get_colore();
     }
-    if((e.errore()).compare("[Eccezione::Abbandono]") == 0){ // gestione abbandono partita
-      fine_partita = "Abbandono";
-      vincitore = giocatore_2.get_colore();
-    }
-  }
-  else if(scacchiera_->stallo(colore_)) //controlla che il giocatore non sia in stallo
-    throw Eccezione("[Eccezione::Patta_Stallo]");
-
-if(scacchiera_->get_ripetizioni_scacchiera() >= 3)
-  if(richiesta_patta())
-    throw Eccezione("[Eccezione::Patta_Posizione]");
-
-
-
-
-bool done = false; //mossa eseguita correttamente
-
-  while(!done){
-    //input della mossa
-
     
-      
-
-  
-  }
-
-  if(scacchiera_->get_conta_mosse() >= 50)
-    throw Eccezione("[Eccezione::Patta_Mosse]");
-
-  if(scacchiera_->get_ripetizioni_scacchiera() >= 3)
-    if(richiesta_patta())
-      throw Eccezione("[Eccezione::Patta_Posizione]");
-  if(scacchiera_->pezzi_insufficienti()){
-    throw Eccezione("[Eccezione::Patta_Insufficenza_di_Pezzi]");
-  }
-  
-  
-    }
-    catch(Eccezione e)
-    {
-      if((e.errore()).compare("[Eccezione::Patta_Stallo]") == 0) // gestione patta per stallo
-        fine_partita = "Patta_Stallo";
-
-      if((e.errore()).compare("[Eccezione::Richiesta_Patta]") == 0) // gestione richiesta patta
-        if(giocatore_2.ricevuta_richiesta_patta())
-          fine_partita = "Patta_Accordo";
-
-      if((e.errore()).compare("[Eccezione::Patta_Materiale]") == 0) // gestione patta per materiale insufficiente
-        fine_partita = "Patta_Insufficienza di materiale";
-      
-      if((e.errore()).compare("[Eccezione::Patta_Posizione]") == 0) // gestione patta posizione ripetuta
-        fine_partita = "Patta_Posizione ripetuta";
-
-      if((e.errore()).compare("[Eccezione::Patta_Mosse]") == 0) // gestione patta
-        fine_partita = "Patta_Gioco fermo (mosse)";
-
-      if((e.errore()).compare("[Eccezione::Scaccomatto]") == 0){ // gestione scaccomatto 
-        fine_partita = "Scaccomatto";
-        vincitore = giocatore_2.get_colore();
-      }
-
-      if((e.errore()).compare("[Eccezione::Abbandono]") == 0){ // gestione scaccomatto 
-        fine_partita = "Abbandono";
-        vincitore = giocatore_1.get_colore();
-      }
-    }
-  }
-
-
-  if((fine_partita.substr(0,4)).compare("Patta") == 0){
-    int const FRASE_PIU_LUNGA =  strlen("Patta_Insufficienza di materiale");
-    for(int i = fine_partita.size(); i< FRASE_PIU_LUNGA; i++)
-      fine_partita += " ";
-    
-    std::cout << std::endl;
-    std::cout << "***************************************************************" << std::endl;
-   // std::cout << "*       La partita si "<<e_accentata<<" conclusa con una patta                *" << std::endl;
-    std::cout << "*       causa: ";
-    std::cout << fine_partita.substr(6);
-    std::cout << "               *" << std::endl;
-    std::cout << "***************************************************************" << std::endl;
-    std::cout << std::endl;
+    std::cout<<"Partita conclusa per: "<<fine_partita;
   }
 }
