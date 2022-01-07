2ba7c74825ae361c7067de36041bf944198e3c7e
mister <cristianbassotto21@gmail.com>
2022-01-07 17:47:58 +0100

    Funziona scaccomatto

--- src/main.cpp
+++ src/main.cpp
@@ -7,54 +7,7 @@
 int main() {
   char e_accentata = 0x00F7;
   Scacchiera test;
-  std::cout<<test.stringa_per_mappa()<<std::endl;
-  /*
-  for(Pezzo* t: test.pezzi_bianchi)
-    std::cout<<t->get_figura();
-  std::cout<<std::endl<<std::endl;
 
-  for(Pezzo* t: test.pezzi_neri)
-    std::cout<<t->get_figura();
-  std::cout<<std::endl;
-  
-  
-  test.mossa(Casella(0,1), Casella(2,2));
-  test.stampa();
-  std::cout << "\n";
-
-  test.mossa(Casella(7,1), Casella(5,2));
-  test.stampa();
-  std::cout << "\n";
-
-  test.mossa(Casella(5,2), Casella(3,3));
-  test.stampa();
-  std::cout << "\n";
-  
-
-  test.mossa(Casella(3,3), Casella(1,4));
-  test.stampa();
-  std::cout << "\n";
-
-  test.mossa(Casella(1,1), Casella(3,1));
-  test.stampa();
-  std::cout << "\n";
-
-  test.mossa(Casella(0,3), Casella(1,4));
-  test.stampa();
-  std::cout << "\n";
-
-  test.mossa(Casella(0,2), Casella(2,0));
-  test.stampa();
-  std::cout << "\n";
-
-  //Casella alfiere(0,5);  
-  //Casella alfiere1(1,4);
-  //test.mossa(alfiere, alfiere1);
-  test.stampa();
-  std::cout << "\n";
-  */
-
-  
   bool colore = static_cast<bool>(rand() % 2); //scelta randomica dei colori dei giocatori
   Umano giocatore_1(&test, static_cast<Pezzo::Colore>(colore));
   Umano giocatore_2(&test, static_cast<Pezzo::Colore>(!colore));
@@ -154,7 +107,6 @@ int main() {
       std::cout << "*       Complimenti giocatore bianco!!                        *" << std::endl;
     else
       std::cout << "*       Complimenti giocatore nero!!                          *" << std::endl;
-    std::cout << "*                                                             *" << std::endl;
     std::cout << "*       Hai vinto la partita per scaccomatto                  *" << std::endl;
     std::cout << "***************************************************************" << std::endl;
     std::cout << std::endl;
