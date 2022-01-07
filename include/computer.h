44e23a87fd9048095d7b849b549d009e24c6779b
mister <cristianbassotto21@gmail.com>
2022-01-07 01:48:54 +0100

    prova

--- include/computer.h
+++ include/computer.h
@@ -13,12 +13,6 @@ class Computer : public Giocatore{
 
     //chiede la mossa finche non ne esegue una
     void gioca();
-
-    //riceve la richiesta di patta fatta dall'avversario
-    bool ricevuta_richiesta_patta();
-
-    //richiede se si vuole eseguire la patta per posizioni ripetute
-    bool richiesta_patta();
 };
 
 #endif // COMPUTER_H
